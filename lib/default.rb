f3c864f9088f3b2724703d1f207c289eb2c60faa
Chad Mikolajcik <chadmiko@gmail.com>
2014-08-06 18:24:13 -0400

    Select don't keep; fix sort

--- lib/default.rb
+++ lib/default.rb
@@ -9,7 +9,6 @@ include Nanoc3::Helpers::XMLSitemap
 include Nanoc3::Helpers::Tagging
 
 def events_by_month(month, year=Date.today.year)
-
   months = if month.is_a?(Array)
     month
   else
@@ -20,11 +19,11 @@ def events_by_month(month, year=Date.today.year)
     next unless i[:kind] == 'event' && i[:start_at] 
 
     begin
-
       dt = Chronic.parse(i[:start_at])
       next unless months.include?(dt.month) && year == dt.year
 
-    rescue 
+    rescue Exception => e 
+      puts e.message 
       next 
     end    
 
@@ -33,7 +32,22 @@ def events_by_month(month, year=Date.today.year)
 
 end
 
+def recent_news(limit=5)
+  @items.select{ |i| i[:kind] == 'article' && i[:publish_at]}.sort do |x, y|
+    begin
+      x_dt = Chronic.parse(x[:publish_at])
+      y_dt = Chronic.parse(y[:publish_at])
+      y_dt <=> x_dt
+    rescue 
+      x[:title] <=> y[:title]
+    end
+  end
+end
+
 def chop_to_html(identifier)
   identifier.chop + ".html"
 end
 
+def carriers
+  @items.select {|i| i[:kind] == 'carrier' }.sort
+end
